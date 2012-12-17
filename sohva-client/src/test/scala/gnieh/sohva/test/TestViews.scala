038398e544f2e2d6d6ecbc38127c80b1c2bcbead
Lucas Satabin <lucas.satabin@gnieh.org>
2012-12-17 19:16:37 +0100

    reorganize directories
    there are now multiple projects:
     - one for the client (formerly sohva)
     - one for the server

---
+++ sohva-client/src/test/scala/gnieh/sohva/test/TestViews.scala
@@ -0,0 +1,52 @@
+/*
+* This file is part of the sohva project.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package gnieh.sohva
+package test
+
+/** @author Lucas Satabin
+ *
+ */
+object TestViews extends App {
+
+  val couch = new CouchClient
+  val session = couch.startSession
+
+  val db = session.database("test")
+
+  session.login("admin", "admin")!
+
+  db.delete!
+
+  db.create!
+
+  session.logout!
+
+  case class TestDoc(_id: String, value: Int)(val _rev: Option[String] = None)
+
+  for (i <- 1 to 10)
+    db.saveDoc(TestDoc("doc:" + i, i * 27)())!
+
+  val design = db.design("toto")
+
+  design.saveView("test", "function(doc){emit(doc._id, doc._id);}")!
+
+  val view = design.view[String, String, TestDoc]("test")
+
+  view.query(include_docs = true).!.rows.foreach(println _)
+
+  couch.shutdown
+
+}
---
+++ sohva-client/src/test/scala/gnieh/sohva/test/User.scala
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the blue project.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gnieh.sohva
+package test
+
+/** A user of blue has the standard fields of couchdb users
+ *  but also contains extra information, specific to blue.
+ *
+ *  @author Lucas Satabin
+ *
+ */
+case class User(name: String,
+                firstName: String,
+                lastName: String,
+                email: String)(
+                    val _rev: Option[String] = None) {
+
+  val _id = "org.couchdb.user:" + name
+
+}
+
+object TestUser extends App {
+
+  val couch = new CouchClient
+  val session = couch.startSession
+
+  session.login("admin", "admin")!
+
+  val db = session.database("blue_users")
+
+  db.create!
+
+  val user = User("lsatabin", "Lucas", "Satabin", "lucas@publications.li")()
+
+  db.saveDoc(user)!
+
+  couch.shutdown
+
+}
