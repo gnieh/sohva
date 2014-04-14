924cf7924341e8c819d60c417e244b84813d460a
Lucas Satabin <lucas.satabin@gnieh.org>
2014-04-14 22:39:01 +0200

    [imp #17] Add raw view API
    This is raw in the sense that objects are not extracted to scala classes
    but instead are sent back as json objects. This can be useful when
    dealing with views that may return different object types. Use it if you
    have no usable (I mean something more specific than `Anyi`) common super
    type for your results and/or documents.
    
    This contributes to #14 and close #17

--- sohva-client/src/main/scala/gnieh/sohva/View.scala
+++ sohva-client/src/main/scala/gnieh/sohva/View.scala
@@ -15,11 +15,35 @@
 */
 package gnieh.sohva
 
+import net.liftweb.json.{
+  JValue,
+  JObject
+}
+
 /** A view can be queried to get the result.
  *
  *  @author Lucas Satabin
  */
 trait View[Result[_]] {
+
+  /** Queries the view on the server and returned the untyped result. */
+  def queryRaw(
+    key: Option[JValue] = None,
+    keys: List[JValue] = Nil,
+    startkey: Option[JValue] = None,
+    startkey_docid: Option[String] = None,
+    endkey: Option[JValue] = None,
+    endkey_docid: Option[String] = None,
+    limit: Int = -1,
+    stale: Option[String] = None,
+    descending: Boolean = false,
+    skip: Int = 0,
+    group: Boolean = false,
+    group_level: Int = -1,
+    reduce: Boolean = true,
+    include_docs: Boolean = false,
+    inclusive_end: Boolean = true,
+    update_seq: Boolean = false): Result[RawViewResult]
 
   /** Queries the view on the server and returned the typed result.
    *  BE CAREFUL: If the types given to the constructor are not correct,
@@ -47,10 +71,23 @@ trait View[Result[_]] {
 
 case class ViewDoc(map: String, reduce: Option[String])
 
+final case class RawViewResult(
+  total_rows: Long,
+  offset: Long,
+  rows: List[RawRow],
+  update_seq: Option[Long])
+
+final case class RawRow(
+  id: Option[String],
+  key: JValue,
+  value: JValue,
+  doc: Option[JObject])
+
 final case class ViewResult[Key, Value, Doc](
     total_rows: Int,
     offset: Int,
-    rows: List[Row[Key, Value, Doc]]) {
+    rows: List[Row[Key, Value, Doc]],
+    update_seq: Option[Int]) {
 
   self =>
 
@@ -68,11 +105,11 @@ final case class ViewResult[Key, Value, Doc](
     rows.foreach(f)
 
   def map[Key1, Value1, Doc1](f: Row[Key, Value, Doc] => Row[Key1, Value1, Doc1]): ViewResult[Key1, Value1, Doc1] =
-    ViewResult(total_rows, offset, rows.map(f))
+    ViewResult(total_rows, offset, rows.map(f), update_seq)
 
   def flatMap[Key1, Value1, Doc1](f: Row[Key, Value, Doc] => ViewResult[Key1, Value1, Doc1]): ViewResult[Key1, Value1, Doc1] = {
     val results = rows.map(f)
-    ViewResult(results.map(_.total_rows).sum, offset, results.flatMap(_.rows))
+    ViewResult(results.map(_.total_rows).sum, offset, results.flatMap(_.rows), update_seq)
   }
 
   def withFilter(p: Row[Key, Value, Doc] => Boolean): WithFilter =
@@ -90,7 +127,7 @@ final case class ViewResult[Key, Value, Doc](
       ViewResult(rows.size, offset, for {
         row <- rows
         if p(row)
-      } yield f(row))
+      } yield f(row), update_seq)
 
     def flatMap[Key1, Value1, Doc1](f: Row[Key, Value, Doc] => ViewResult[Key1, Value1, Doc1]): ViewResult[Key1, Value1, Doc1] = {
       val rows1 = for {
@@ -98,7 +135,7 @@ final case class ViewResult[Key, Value, Doc](
         if p(row)
         row1 <- f(row).rows
       } yield row1
-      ViewResult(rows1.size, offset, rows1)
+      ViewResult(rows1.size, offset, rows1, update_seq)
     }
 
     def withFilter(q: Row[Key, Value, Doc] => Boolean): WithFilter =
