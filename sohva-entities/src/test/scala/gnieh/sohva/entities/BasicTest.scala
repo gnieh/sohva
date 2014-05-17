29f46f0a5854807b5ce84e265ccf4781cacee068
Lucas Satabin <lucas.satabin@gnieh.org>
2014-05-17 17:00:25 +0200

    Add basic test case

--- sohva-entities/src/test/scala/gnieh/sohva/entities/BasicTest.scala
+++ sohva-entities/src/test/scala/gnieh/sohva/entities/BasicTest.scala
@@ -6,34 +6,58 @@ import async._
 import akka.actor.ActorSystem
 import akka.util._
 
+import scala.concurrent._
 import scala.concurrent.duration._
 
-object BasicTest extends App {
+import org.scalatest._
 
-  implicit val timeout = Timeout(20.seconds)
-  implicit val system = ActorSystem("sohva")
+class BasicTest extends FlatSpec with ShouldMatchers with BeforeAndAfterAll {
 
   import system.dispatcher
 
+  implicit val system = ActorSystem()
+  implicit val timeout = Timeout(5.seconds)
+
   val couch = new CouchClient
   val session = couch.startCookieSession
+  val db =  session.database("sohva-entities-tests")
 
-  val f = for {
-    true <- session.login("admin", "admin")
-    db = session.database("entities")
-    _ <- db.create
-    manager = new EntityManager(db)
-    entity <- manager.createTagged("Test Entity")
-    comp1 = Component1("gruik", 3, "my first component")
-    true <- manager.addComponent(entity, comp1)
-    comp2 <- manager.getComponent[Component1](entity)
-    true <- db.delete
-  } yield comp2
+  "It" should "be possible to create and manage entities with a manager" in {
+    val comp = Component1("gruik", 3, "my first component")
 
-  f.onComplete {
-    case res =>
-      println(res)
-      system.shutdown()
+    val f = for {
+      _ <- db.create
+      manager = new EntityManager(db)
+      entity <- manager.createTagged("Test Entity")
+      comp1 = Component1("gruik", 3, "my first component")
+      true <- manager.addComponent(entity, comp1)
+      comp2 <- manager.getComponent[Component1](entity)
+      true <- db.delete
+    } yield comp2
+
+    val res = Await.result(f, Duration.Inf)
+
+    res should be('defined)
+    val result = res.get
+    result should be(comp)
+    result._rev should be('defined)
+
+  }
+
+  override def beforeAll() {
+    // login
+    session.login("admin", "admin")
+    // create database
+    db.create
+  }
+
+  override def afterAll() {
+    // cleanup database
+    db.delete
+    // logout
+    session.logout
+    couch.shutdown()
+    system.shutdown()
   }
 
 }
